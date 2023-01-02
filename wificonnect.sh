0e30f523dfa5818152e6b0c75766bad84df10459
Raudel <94656743+tc34d@users.noreply.github.com>
2023-01-02 14:23:23 -0500

    Update wificonnect.sh

--- wificonnect.sh
+++ wificonnect.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Check if the required command-line arguments were provided
-if [ "$#" -ne 2 ]; then
-  echo "Usage: connect WIFI_SSID WIFI_PASSWORD"
-  exit 1
+# Scan for available wireless networks
+sudo iw wlan0 scan | grep SSID
+
+echo -n "Enter the SSID of the network you want to connect to: "
+read ssid
+
+# Connect to the wireless network
+sudo iw wlan0 connect "$ssid"
+
+# Get the gateway IP from the DHCP server
+echo dhcpd wlan0 | grep 'router'
+
+# Set the network as the default route
+ip route add default via "$gateway" dev wlan0
+
+# Check if the connection was successful by pinging a website
+if ping -c 1 google.com; then
+  echo "Successfully connected to the internet!"
+else
+  echo "Failed to connect to the internet. Please check your network settings."
 fi
-
-# Store the SSID and password in variables
-ssid=$1
-password=$2
-
-# Connect to the WiFi network
-nmcli device wifi connect "$ssid" password "$password"
