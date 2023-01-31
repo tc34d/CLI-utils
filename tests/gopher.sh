38440a402c05c6b691aab53de8204150397d7058
tc34d <raudel.rubio20@gmail.com>
2023-01-31 13:41:06 -0500

    hotfix

--- tests/gopher.sh
+++ tests/gopher.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-os==cat /etc/os-release | grep ID
+os==cat /etc/os-release | grep name
+if $os == "Arch Linux"; then
+    sudo pacman -Syu lynx
+elif $os == "Fedora"; then
+    dnf install lynx
+elif $os == "Debian GNU/Linux"; then
+    sudo apt-get install lynx
 
 lynx gopher://gopher.floodgap.com
