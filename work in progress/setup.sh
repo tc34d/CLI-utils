ea7ba7e2f3dd85a7d3859ab131a15a2042398f50
tc34d <raudel.rubio20@gmail.com>
2023-02-01 17:03:02 -0500

    did some upgrades

--- work in progress/setup.sh
+++ work in progress/setup.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
-# full setup script for ArchLinux
+#network
+echo would you like to connect to the wifi y/n
+read bean
+if bean == "y" then;
+sudo pacman -S network-manager
+sudo iwconfig wlan0 up
+nmcli radio wifi on
+nmcli radio wifi
+sudo iw wlan0 scan | grep SSID
+read input
+sudo nmcli --ask dev wifi connect $input 
+else
+echo continuing through ethernet
+fi
+
+# installing common packages
 if sudo pacman -Qi neofetch vi vim  > /dev/null ;then
-    echo packages already installed continueing with script
+    echo "packages already installed continueing with script"
 else
  yes | sudo pacman -S neofetch vi vim 
 
 fi
+
 if sudo pacman -Qi firefox libreoffice > /dev/null ;then
-    echo would you like to install firefox and libreoffice y/n
+    echo "would you like to install firefox and libreoffice y/n"
     read option
     if [ $option=="y" ]; then
         sudo pacman -S firefox libreoffice
     else
-        echo skipping install of firefox and libreoffice
+        echo "skipping install of firefox and libreoffice"
     fi
 fi
+sudo pacman -Syyu
+echo "step finnished"
+echo -e
+echo "type in any other packages that you want to install"
+read packages
+sudo pacman -S $packages
