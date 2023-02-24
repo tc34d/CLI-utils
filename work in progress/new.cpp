ec06f171a5b1b8927c19322073dcb2a559326dd4
tc34d <raudel.rubio20@gmail.com>
2023-02-24 17:32:52 -0500

    mn

---
+++ work in progress/new.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+int main(){
+float n;
+std::cout<<"input password length\n";
+std::cin>>n;
+while(int(n)>0){
+
+
+
+}
+
+}
