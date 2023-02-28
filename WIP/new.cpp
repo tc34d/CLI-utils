f7e0dffb5604c9fcd9634618392b768caddb5e9b
tc34d <raudel.rubio20@gmail.com>
2023-02-28 07:30:44 -0500

    sync

---
+++ WIP/new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+int main(int n){
+while(n>0){
+std::cout<<std::rand() % 10;
+n--;
+}
+
+}
--- network/gopher2beta.sh
+++ network/gopher2beta.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-var=$=cat /etc/os-release | grep name
 
-#the first if an elif statements check what distro is running 
-#the nested if statements check if lynx is installed if ti is it runs the gopher thing
-
-if $ '=' "Arch Linux"; then
+if distro | grep Name == "Name: Arch Linux"; then
     if sudo pacman -Qi lynx > /dev/null; then
         lynx gopher://gopher.floodgap.com
     fi
     sudo pacman -Syu lynx
     
-elif $ '=' "Fedora"; then
+elif distro | grep Name == "Name: Fedora"; then
     if sudo dnf list installed | grep lynx >/dev/null; then
         echo whatis
     fi
     dnf install lynx
     
-elif $ '=' "Debian GNU/Linux"; then
+elif distro | grep Name "Name: Debian GNU/Linux"; then
 
     sudo apt-get install lynx
 
